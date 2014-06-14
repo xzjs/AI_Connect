57f9a1b64013410d6feaae4e1109a8ca3ff900f6
everythingbeen <1361419239@qq.com>
2014-06-15 02:20:15 +0800

    初次代

--- src/com/org/ouc/ConnectSix/zuobiao.java
+++ src/com/org/ouc/ConnectSix/zuobiao.java
@@ -6,4 +6,9 @@ package com.org.ouc.ConnectSix;
 public class zuobiao {
     public int x;
     public int y;
+     public zuobiao(int a,int b)
+     {
+         this.x=a;
+         this.y=b;
+     }
 }
